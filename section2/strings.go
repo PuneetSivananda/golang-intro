84158234a29af65dc1dd3da4d8e2eb9b87c41f8b
PuneetSivananda <psivananda@chatsoft.com>
2021-05-02 00:20:38 +0530

    feat: string functions

--- section2/strings.go
+++ section2/strings.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+/*
+All strings are Mutable in go
+*/
 func main(){
-	fmt.Println("Hello World")
+	m1 := "My Name"
+
+	fmt.Println(strings.Split(m1, " "))
 }
