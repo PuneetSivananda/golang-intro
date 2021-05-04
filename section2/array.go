4765256ab2fd2a09416bee592b27d72f7113490f
PuneetSivananda <psivananda@chatsoft.com>
2021-05-04 13:05:04 +0530

    feat: array append

--- section2/array.go
+++ section2/array.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func todo(){
 	//var arr[] int
-	arr := []int{2,4,6,8}
-	fmt.Println(arr)
+	arr1 := []int{2,4,6,8}
+	arr2 := []string{"Hi","My","Name","is","Rudolph"}
+	// append function adds an item to the array
+	arr2 = append(arr2, "!!")
+	fmt.Println(arr1, arr2)
 }
 
 func main(){
