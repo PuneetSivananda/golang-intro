1da646858f00f94cf312012686c9c12fe87f0401
PuneetSivananda <psivananda@chatsoft.com>
2021-05-08 00:20:16 +0530

    control structs

---
+++ section2/controls.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Foo bar")
+	fmt.Println("Hello World")
+
+	flag:= true
+	
+	if flag{
+		fmt.Println("True")
+	}	else{
+		fmt.Println("False")
+	}
+	
+}
