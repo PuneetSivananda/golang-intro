4f1fba12fda829af45079d0122ae96638ba1fe76
PuneetSivananda <psivananda@chatsoft.com>
2021-05-07 13:56:53 +0530

    wip: interfaces

--- section2/interface-2.go
+++ section2/interface-2.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func Anything(anything interface{}){
+	fmt.Println(anything)
+}
+
 func main(){
 	fmt.Println("Interfaces contd")
+	Anything(2.4)
+	Anything("Hello")
+	Anything(struct{}{})
 }
