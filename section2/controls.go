07b0f9ef5e54d3b609d573b41f735bfccfaef463
PuneetSivananda <psivananda@chatsoft.com>
2021-05-08 00:48:51 +0530

    looping over maps

--- section2/controls.go
+++ section2/controls.go
@@ -30,5 +30,29 @@ func main(){
 		fmt.Println("False")
 	}
 
-	
+	// Looping
+	for i:= 0; i<10; i++{
+		fmt.Println(i)
+	}
+	// infinite loop
+	// for {
+	// 	fmt.Println("Text")
+	// }
+
+	name:=[]string{"Rob", "Van", "Mike"}
+	for item:= range(name){
+		fmt.Println(item, name[item])
+	}
+
+
+	//maps
+
+	mymap := make(map[string]interface{})
+	mymap["name"] = "Testing"
+	mymap["age"] = 20
+
+	for k, v := range(mymap){
+		fmt.Printf("Key is: %s and value is: %v", k, v)
+		fmt.Println()
+	}
 }
