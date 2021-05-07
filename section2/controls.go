efbbe2c80b01d56a96bf5cb27ffec36b30aa0329
PuneetSivananda <psivananda@chatsoft.com>
2021-05-08 00:31:41 +0530

    feat: control statements

--- section2/controls.go
+++ section2/controls.go
@@ -8,10 +8,27 @@ func main(){
 
 	flag:= true
 	
-	if flag{
+	if flag && 2 ==5 || 4 ==4{
 		fmt.Println("True")
 	}	else{
 		fmt.Println("False")
 	}
+
+	f := &flag
+	
+	if *f{
+		fmt.Println("True")
+	} else if !*f {
+		fmt.Println("False")
+	}
+
+	if f == nil {
+		fmt.Println("Value is nil")
+	} else if *f {
+		fmt.Println("True")
+	}	else{
+		fmt.Println("False")
+	}
+
 	
 }
