c8600e27099dffc4dc89a980026f9a06973d477d
PuneetSivananda <psivananda@chatsoft.com>
2021-05-08 12:20:26 +0530

    feat: conditionals

--- section2/controls-2.go
+++ section2/controls-2.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi")
+	fmt.Println("Switch Case")
+	//Continue, break, switch
+
+	for i:=0; i<10; i++{
+		if i%2==0{
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	for i:=1; i<10; i++{
+		if i%2==0{
+			break
+		}
+		fmt.Println(i)
+	}
+
+	day:= "Fri"
+
+	switch day {
+	case "Fri":
+		fmt.Println("TGIF")
+	case "Mon", "Tue", "Wed", "Thu":
+		fmt.Println("Boring")
+	}	
 }
