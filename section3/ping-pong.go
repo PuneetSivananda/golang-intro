cd4eb86d9a7f554fccf53e46ddcddd71ddcbe5d8
PuneetSivananda <psivananda@chatsoft.com>
2021-05-10 02:07:45 +0530

    feat: ping pong channel tut

--- section3/ping-pong.go
+++ section3/ping-pong.go
@@ -5,7 +5,43 @@ import (
 	"time"
 )
 
+// The Pinger print a ping and waits for a pong
+func pinger(pinger <-chan int, ponger chan<- int){
+	for{
+		<- pinger
+		fmt.Println("PING")
+		time.Sleep(time.Second *1)
+		ponger<- 1
+	}
+}
+
+// The Ponger print a pong and waits for a ping
+func ponger(pinger chan<- int, ponger <-chan int){
+	for{
+		<-ponger
+		fmt.Println("PONG")
+		time.Sleep(time.Second *1)
+		pinger<- 1
+	}
+}
+
+
 func main(){
 	fmt.Println("Ping Ping in GO")
-	time.Sleep(time.Second * 1)
+		
+	ping:= make(chan int)
+	pong:= make(chan int)
+
+	go pinger(ping, pong)
+	go ponger(ping, pong)
+	
+	// Initialize the ping 
+	ping<-1
+	// for{
+	// 	time.Sleep(time.Second)
+	// }
+
+	// indefinate loop
+	select{}
+
 }
