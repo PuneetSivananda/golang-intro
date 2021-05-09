4cdc46c12ab367339be0a96f1905dc0a383d3ae4
PuneetSivananda <psivananda@chatsoft.com>
2021-05-10 01:48:21 +0530

    feat: added addressing

--- section3/buf-channel.go
+++ section3/buf-channel.go
@@ -11,12 +11,12 @@ type Car struct{
 
 func main(){
 	fmt.Println("Buffered Channels")
-	c:= make(chan Car, 3)
+	c:= make(chan *Car, 3)
 	go func(){
-		c<- Car{"1"}
-		c<- Car{"2"}
-		c<- Car{"3"}
-		c<- Car{"4"}
+		c<- &Car{"1"}
+		c<- &Car{"2"}
+		c<- &Car{"3"}
+		c<- &Car{"4"}
 		close(c)
 	}()
 
