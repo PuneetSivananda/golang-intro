eac62f717076686ccb591fe83086061d884abc0f
PuneetSivananda <psivananda@chatsoft.com>
2021-05-09 21:04:17 +0530

    feat: goroutines explained

--- section3/goroutine.go
+++ section3/goroutine.go
@@ -13,9 +13,18 @@ func heavy(){
 	}
 }
 
+func superHeavy(){
+	for{
+		fmt.Println("Super Heavy function")
+		time.Sleep(time.Second * 2)
+	}
+
+}
+
 func main(){
 	// heavy() // normal function
 	go heavy() //goroutine
+	go superHeavy()
 	fmt.Println("VSCODE go")
-	time.Sleep(time.Second * 5)
+	select{}
 }
