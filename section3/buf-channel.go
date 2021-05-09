f7d9f7b3ee2d87934e4f775fed93edfe310c1d7a
PuneetSivananda <psivananda@chatsoft.com>
2021-05-10 01:47:37 +0530

    feat: using custom structs

--- section3/buf-channel.go
+++ section3/buf-channel.go
@@ -5,22 +5,25 @@ import (
 	"fmt"
 	"time"
 )
+type Car struct{
+	Model string
+}
 
 func main(){
 	fmt.Println("Buffered Channels")
-	c:= make(chan int, 3)
+	c:= make(chan Car, 3)
 	go func(){
-		c<- 1
-		c<- 2
-		c<- 3
-		c<- 4
+		c<- Car{"1"}
+		c<- Car{"2"}
+		c<- Car{"3"}
+		c<- Car{"4"}
 		close(c)
 	}()
-	
+
 	time.Sleep(time.Second *1)
 
 	for i:= range(c){
-		fmt.Println(i)
+		fmt.Println(i.Model)
 	}
 	
 }
