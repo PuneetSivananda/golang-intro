e708b760397922fd6822b3a8ceaf660385623ea6
PuneetSivananda <psivananda@chatsoft.com>
2021-05-10 22:17:02 +0530

    feat:MVC

--- section4/main.go
+++ section4/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"./structs"
+	"./views"
 )
 
 func main(){
@@ -13,7 +13,7 @@ func main(){
 
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request){
 		if r.Method == http.MethodGet{
-			data:= structs.Response{
+			data:= views.Response{
 				Code: http.StatusOK,
 				Body: "pong",
 			}
@@ -21,6 +21,6 @@ func main(){
 			json.NewEncoder(w).Encode(data)
 		}
 	})
-	
+
 	http.ListenAndServe("localhost:3000", mux)
 }
