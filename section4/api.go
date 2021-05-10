95eb3e78ef5836f08f242f988afb7fd1fc519fc4
PuneetSivananda <psivananda@chatsoft.com>
2021-05-10 18:08:52 +0530

    feat: api requests

--- section4/api.go
+++ section4/api.go
@@ -1,2 +1,16 @@
 // API Application Programming interface;
-// MVC Model View Controller architechture
+// MVC Model View Controller
+
+package main
+
+import (
+	"net/http"
+)
+
+func main(){
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe("localhost:3000", mux)
+}
