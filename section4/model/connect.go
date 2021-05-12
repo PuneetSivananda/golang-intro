54fded17e44cbbdacda14fb937c691600385f37d
PuneetSivananda <psivananda@chatsoft.com>
2021-05-12 18:08:06 +0530

    feat: add a row func

--- section4/model/connect.go
+++ section4/model/connect.go
@@ -12,7 +12,7 @@ var con *sql.DB
 
 func Connect() *sql.DB{
 	// db, err:= sql.Open("sqlite3", "../todo.db")
-	db, err:= sql.Open("sqlite3", ":memory:")
+	db, err:= sql.Open("sqlite3", "../todo.db")
 	if err != nil{
 		log.Fatal(err)
 	}
--- section4/model/create.go
+++ section4/model/create.go
@@ -1,13 +1,40 @@
 package model
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"	"log"
+)
+
 
 func CreateTODO() error {
-	insertQ, err := con.Exec("INSERT INTO TODO VALUES(?, ?)", "Puneet", "Todo1")
+	// tx, _ := con.Begin()
+	result, err := con.Exec("insert into TODO(name, todo) values(?, ?)", "Puneet", "Todo1")
+	fmt.Println(result)
+	// tx.Commit()
 	defer con.Close()
 	if err != nil {
-		fmt.Println(insertQ)
 		return err
 	}
 	return nil
+	// insertQ, err := con.Exec("INSERT INTO TODO VALUES(?, ?)", "Puneet", "Todo1")
+	// defer con.Close()
+	// if err != nil {
+	// 	fmt.Println(insertQ)
+	// 	return err
+	// }
+	// fmt.Println(insertQ)
+	// return nil
+}
+
+func insertStudent(db *sql.DB, code string, name string, program string) {
+	log.Println("Inserting student record ...")
+	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
+	statement, err := db.Prepare(insertStudentSQL) // Prepare statement. This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(code, name, program)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
