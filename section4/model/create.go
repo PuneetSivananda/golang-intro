e46e68257263da0e84eff8848c149d816202720c
PuneetSivananda <psivananda@chatsoft.com>
2021-05-13 17:03:36 +0530

    feat: read all

--- section4/model/create.go
+++ section4/model/create.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 )
@@ -11,31 +10,9 @@ func CreateTODO(name, todo string) error {
 	fmt.Println("--->", name, todo)
 	_, err := con.Exec("insert into TODO(name, todo) values(?, ?)", name, todo)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return err
 	}
-	defer con.Close()
 	return nil
-	
-	// insertQ, err := con.Exec("INSERT INTO TODO VALUES(?, ?)", "Puneet", "Todo1")
-	// defer con.Close()
-	// if err != nil {
-	// 	fmt.Println(insertQ)
-	// 	return err
-	// }
-	// fmt.Println(insertQ)
-	// return nil
 }
 
-func insertStudent(db *sql.DB, code string, name string, program string) {
-	log.Println("Inserting student record ...")
-	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
-	statement, err := db.Prepare(insertStudentSQL) // Prepare statement. This is good to avoid SQL injections
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	_, err = statement.Exec(code, name, program)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-}
